01a627443e185f3113511dc1a700e2b50b61efd9
C110181141 <C110181141@nkust.edu.tw>
2021-11-09 16:45:58 +0800

    This is my first commit

--- untitled/src/bmi.java
+++ untitled/src/bmi.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class bmi {
-}
+public class bmi {
+        public static void main(String[] args) {
+            float high = 1.72f;
+            int  weight = 55;
+            System.out.println (weight /( high * high ));
+
+        }
+    }
+
