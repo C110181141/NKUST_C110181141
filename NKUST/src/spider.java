25c730c7b819598948103e59cae52b3977900e4f
C110181141 <C110181141@nkust.rdu.tw>
2021-11-30 16:30:06 +0800

    This is my first commit

--- NKUST/src/spider.java
+++ NKUST/src/spider.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class spider {
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class spider {
+    public static void main(String[] args){
+       try{
+        Document doc = Jsoup.connect("https://rule.nkust.edu.tw/").get();
+        System.out.println(doc.title());
+        Elements newsHeadlines = doc.select("#Dyn_2_2 a");
+        for (Element headline : newsHeadlines) {
+            System.out.println(headline.attr("title"));
+            System.out.println(headline.absUrl("href"));
+     }
+    } catch(Exception e){
+        System.out.println(e);
+       }
+    }
 }
+
