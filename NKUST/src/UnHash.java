aa2044418e279e31e93b1c4dab60a7cbc77efbd9
C110181141 <93572225+C110181141@users.noreply.github.com>
2021-12-07 00:39:22 +0800

    Update UnHash.java

--- NKUST/src/UnHash.java
+++ NKUST/src/UnHash.java
@@ -2,7 +2,7 @@ public class UnHash {
 
     public static void main (String[] args) {
             System.out.println(revHash(6933552791181934L));
-            System.out.println(hash("justdoit")); //574318821802
+            //System.out.println(hash("justdoit")); //574318821802
         }
 
         public static String letters = "cdefghijlmnoqstuvxz";
@@ -24,12 +24,11 @@ public class UnHash {
             return String.valueOf((english.reverse()));
         }
 
-        public static long hash(String s){
-            long h = 7;
-            for (int i = 0; i < s.length(); i++){
-                h = h * 23 + letters.indexOf(s.charAt(i));
-            }
-            return h;
-        }
+        //public static long hash(String s){
+           // long h = 7;
+           // for (int i = 0; i < s.length(); i++){
+               // h = h * 23 + letters.indexOf(s.charAt(i));
+           // }
+           // return h;
+        //}
     }
-
