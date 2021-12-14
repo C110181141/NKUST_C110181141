17b311a9911774f95d52e511b233b0a77158571e
C110181141 <93572225+C110181141@users.noreply.github.com>
2021-12-14 16:28:29 +0800

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,27 +1,38 @@
 public class Person {
-    int hp;
-    int Att;
-    int Mp;
-    String name; //data member
+        int hp; //data member
+        int att; //data member
+        int mp; //data member
+        int attack;
+        String name; //data member
 
-    int gethp() {
-        //method member
-        return this.hp; //this 指向此類別本身
-    }
-    int getAtt() {
-        //method member
-        return this.Att; //this 指向此類別本身
-    }
-    int getMp() {
-        //method member
-        return this.Mp; //this 指向此類別本身
-    }
-    String getMyName() {
-        return this.name; //this 指向此類別本身
+        int gethp() {
+            //method member
+            return this.hp; //this 指向此類別本身
+        }
+        int getAtt(){
+            return this.att;
+        }
+        int getmp(){
+            return this.mp;
+        }
+        int getAttack(){
+            return this.attack;
+        }
+        void attack(Person enemy){
+            int result = this.attack - enemy.attack;
+            if(result>0){
+                this.hp = this.hp - result;
+            }
+        }
+
+        String getMyName() {
+            return this.name;
+        }
+        //this 指向此類別本身
+
+        void ShoutMyName() {
+            //method member
+            System.out.println("I am " + getMyName().toUpperCase() + "."); //於類別內呼叫getMyName函數
+        }
     }
 
-    void ShoutMyName() {
-        //method member
-        System.out.println("I am " + getMyName().toUpperCase() + "."); //於類別內呼叫getMyName函數
-    }
-}
